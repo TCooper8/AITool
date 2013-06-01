b81b3b9f995bbfc225db53b90f318e52ff06ff88
Moghedrin <nolat301@gmail.com>
2013-05-31 23:05:09 -0600

    Added pretty messages to constructer and destructor methods

--- AITool/AITool/FunctionEngine/base.h
+++ AITool/AITool/FunctionEngine/base.h
@@ -1,26 +1,57 @@
 #include <string>
 #include <forward_list>
 #include <unordered_map>
+#include "../Output/out.h"
 #pragma once
 
 class Symbol {
 	public:
 		std::string Name;
-		Symbol() : Name("NIL") {}
-		Symbol(const std::string& n) : Name(n) {}
+		Symbol() : Name("NIL") {
+			out::notify << "Constructed Symbol" << std::endl;
+		}
+		Symbol(const std::string& n) : Name(n) {
+			out::notify << "Constructed Symbol" << std::endl;
+		}
+		~Symbol() {
+			out::report << "Destroying Symbol" << std::endl;
+		}
 		virtual bool Literal() const {return true;}
 };
 
 class SymbolList {
-	std::forward_list<Symbol *> List;
-	SymbolList() {}
-	SymbolList(const std::forward_list<Symbol *>& l) : List(l) {}
+	public:
+		std::forward_list<Symbol *> List;
+		SymbolList() {
+			out::notify << "Constructed Symbol List" << std::endl;
+		}
+		SymbolList(const std::forward_list<Symbol *>& l) : List(l) {
+			out::notify << "Constructed Symbol List" << std::endl;
+		}
+		~SymbolList() {
+			out::report << "Destroying Symbol List" << std::endl;
+		}
 };
 
-class SymbolTable : public std::unordered_map<std::string, Symbol *> {};
+class SymbolTable : public std::unordered_map<std::string, Symbol *> {
+	public:
+		SymbolTable() {
+			out::notify << "Constructed Symbol Table" << std::endl;
+		}
+		virtual ~SymbolTable() {
+			out::report << "Destroying Symbol Table" << std::endl;
+		}
+};
 
-class interpreter {
+class Interpreter {
 	private:
 		SymbolTable Table;
 		std::forward_list<Symbol *> Mem;
+	public:
+		Interpreter() {
+			out::notify << "Constructed Interpreter" << std::endl;
+		}
+		~Interpreter() {
+			out::report << "Destroying Interpreter" << std::endl;
+		}
 };
